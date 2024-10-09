a5752f529d972a6a7b65ebd93f4b1548ed6d509f
Austin Passy <367897+thefrosty@users.noreply.github.com>
2024-10-09 16:07:18 -0700

    Code cleanup.

--- js/color-picker.js
+++ js/color-picker.js
@@ -1,5 +1,5 @@
-jQuery(document).ready(function (jQuery) {
-
-  jQuery('#timeline_um_middle_circle_bg, #timeline_um_middle_line_bg, #timeline_um_items_content_color, #timeline_um_cart_bg, #timeline_um_items_title_color, #timeline_um_items_price_color, #timeline_um_cart_text_color, #timeline_um_slider_pagination_bg, #timeline_um_slider_pagination_text_color').wpColorPicker()
-
+jQuery(document).ready(function ($) {
+  if (typeof wpColorPicker !== 'undefined') {
+    $('#timeline_um_middle_circle_bg, #timeline_um_middle_line_bg, #timeline_um_items_content_color, #timeline_um_cart_bg, #timeline_um_items_title_color, #timeline_um_items_price_color, #timeline_um_cart_text_color, #timeline_um_slider_pagination_bg, #timeline_um_slider_pagination_text_color').wpColorPicker()
+  }
 })
